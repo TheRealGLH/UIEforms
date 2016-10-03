3462c75345567a90f511ae441c0b88891c94fd9b
Tim Goes <Tim_Goes4@hotmail.nl>
2016-10-03 13:55:58 +0200

    gameList form toegevoegd

---
+++ design_forms/gameList.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace design_forms
+{
+    partial class gameList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvcTitel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvcDeveloper = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvcRating = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvcBanner = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnFavorites = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.pbIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvcTitel,
+            this.lvcDeveloper,
+            this.lvcRating,
+            this.lvcBanner});
+            this.listView1.Location = new System.Drawing.Point(12, 120);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(717, 385);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // lvcTitel
+            // 
+            this.lvcTitel.Text = "Titel";
+            this.lvcTitel.Width = 283;
+            // 
+            // lvcDeveloper
+            // 
+            this.lvcDeveloper.Text = "Developer";
+            this.lvcDeveloper.Width = 174;
+            // 
+            // lvcRating
+            // 
+            this.lvcRating.Text = "Rating";
+            this.lvcRating.Width = 149;
+            // 
+            // lvcBanner
+            // 
+            this.lvcBanner.Text = "Banner";
+            this.lvcBanner.Width = 103;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(26, 85);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(107, 85);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(188, 85);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnFavorites
+            // 
+            this.btnFavorites.Location = new System.Drawing.Point(269, 85);
+            this.btnFavorites.Name = "btnFavorites";
+            this.btnFavorites.Size = new System.Drawing.Size(122, 23);
+            this.btnFavorites.TabIndex = 4;
+            this.btnFavorites.Text = "Add To Favorites";
+            this.btnFavorites.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(654, 85);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnMenu.Location = new System.Drawing.Point(325, 520);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(100, 23);
+            this.btnMenu.TabIndex = 6;
+            this.btnMenu.Text = "Back To Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
+            // 
+            // pbIcon
+            // 
+            this.pbIcon.Location = new System.Drawing.Point(325, 12);
+            this.pbIcon.Name = "pbIcon";
+            this.pbIcon.Size = new System.Drawing.Size(100, 67);
+            this.pbIcon.TabIndex = 7;
+            this.pbIcon.TabStop = false;
+            // 
+            // gameList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.pbIcon);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnFavorites);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listView1);
+            this.Name = "gameList";
+            this.Text = "gameList";
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader lvcTitel;
+        private System.Windows.Forms.ColumnHeader lvcDeveloper;
+        private System.Windows.Forms.ColumnHeader lvcRating;
+        private System.Windows.Forms.ColumnHeader lvcBanner;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnFavorites;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.PictureBox pbIcon;
+    }
+}
